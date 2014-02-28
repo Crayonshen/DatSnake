400e6e9cc6413aeaf297db7446236a259a1384e1
Crayonshen <dshen.mic@gmail.com>
2014-02-28 15:41:21 -0800

    snake head

--- src/datsnake/LOL.java
+++ src/datsnake/LOL.java
@@ -5,6 +5,8 @@
 package datsnake;
 
 import static datsnake.Direction.UP;
+import java.awt.Color;
+import java.awt.Graphics;
 import java.awt.Point;
 import java.util.ArrayList;
 
@@ -13,46 +15,93 @@ import java.util.ArrayList;
  * @author David S
  */
 public class LOL {
-    private ArrayList<Point> body;
-    private Direction direction = Direction.RIGHT;
+
     
     {
-    body = new ArrayList<Point>();
-    }
-
-    public void kimchi(){
-        //create a new location for the head, using the direction.
-        int x=0;
-        int y=0;
-        switch (getDirection()){
-            case UP:
-                x=0;
-                y=-1;
-                for (int i = 1; i < 10; i++) {
-                    
-                }
-                break;
-            case DOWN:
-                x=0;
-                y=1;
-                break;
-            case LEFT:
-                x=-1;
-                y=0;
-                break;
-            case RIGHT:
-                x=1;
-                y=0;
-                
-        }
-        body.add(0, new Point(getHead().x+x, getHead().y+y));
-        
-        body.remove(body.size() - 1);
+        body = new ArrayList<Point>();
     }
     
-    public Point getHead(){
+    public void drawHead(Graphics graphics, Point position, Point size){
+        int startAngle = 30;
+        
+        if (getDirection() == Direction.RIGHT){
+            startAngle = 30;             
+        } else if (getDirection() == Direction.LEFT){
+            startAngle = 210;             
+        } else if (getDirection() == Direction.UP){
+            startAngle = 120;             
+        } else if (getDirection() == Direction.DOWN){
+            startAngle = 300;             
+        } 
+        
+        graphics.setColor(getColor());
+        graphics.fillArc(position.x, position.y, size.x, size.y, startAngle, 300);
+        
+        //draw eye
+        graphics.setColor(Color.BLACK);
+        if ((direction == Direction.LEFT) || (direction == Direction.RIGHT)){
+            graphics.fillOval(position.x + (size.x / 2), position.y + (size.y / 4), size.x / 8, size.y / 8);
+        } else if (direction == Direction.UP) {
+            graphics.fillOval(position.x + (size.x / 4), position.y + (size.y / 2), size.x / 8, size.y / 8);
+        } else if (direction == Direction.DOWN) {
+            graphics.fillOval(position.x + (size.x  * 2 / 3), position.y + (size.y / 2), size.x / 8, size.y / 8);
+        }
+    }
+
+    
+    
+
+    public void move() {
+        //create a new location for the head, using the direction.
+        int x = 0;
+        int y = 0;
+        switch (getDirection()) {
+            case UP:
+                x = 0;
+                y = -1;
+                break;
+            case DOWN:
+                x = 0;
+                y = 1;
+                break;
+            case LEFT:
+                x = -1;
+                y = 0;
+                break;
+            case RIGHT:
+                x = 1;
+                y = 0;
+
+        }
+        body.add(0, new Point(getHead().x + x, getHead().y + y));
+
+        if (growthCounter > 0) {
+            growthCounter--;
+            
+        } else {
+            body.remove(body.size() - 1);
+        }
+    }
+
+    public Point getHead() {
         return body.get(0);
     }
+    public Point getTail(){
+        return body.get(body.size()-1);
+    }
+    public boolean selfHitTest(){
+        for (int i = 1; i < body.size(); i++) {
+            if (body.get(i).equals(getHead() )) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private ArrayList<Point> body;
+    private Direction direction = Direction.RIGHT;
+    private int growthCounter;
+    private Color color = Color.PINK;
     /**
      * @return the body
      */
@@ -79,5 +128,40 @@ public class LOL {
      */
     public void setDirection(Direction direction) {
         this.direction = direction;
+    }
+
+    /**
+     * @return the growthCounter
+     */
+    public int getGrowthCounter() {
+        return growthCounter;
+    }
+
+    /**
+     * @param growthCounter the growthCounter to set
+     */
+    public void setGrowthCounter(int growthCounter) {
+        this.growthCounter = growthCounter;
+    }
+
+    /**
+     * @param growthCounter the growthCounter to set
+     */
+    public void grow(int growth) {
+        this.growthCounter += growth;
+    }
+
+    /**
+     * @return the color
+     */
+    public Color getColor() {
+        return color;
+    }
+
+    /**
+     * @param color the color to set
+     */
+    public void setColor(Color color) {
+        this.color = color;
     }
 }
--- src/datsnake/SnakeEnvironment.java
+++ src/datsnake/SnakeEnvironment.java
@@ -5,86 +5,151 @@
 package datsnake;
 
 import environment.Environment;
+import environment.GraphicsPalette;
 import environment.Grid;
+import image.ResourceTools;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
 /**
  *
  * @author David S
  */
-class SnakeEnvironment extends Environment{
-    private Grid tofu;
+class SnakeEnvironment extends Environment {
+
+    private GameState gameState = GameState.PAUSED;
+    private Grid grid;
     private int score = 0;
-    private LOL bored;
-    
-    
+    private LOL snake;
+    private ArrayList<Point> apples;
+    private Image tofu;
+    private ArrayList<Point> tofus;
+    private Image loser;
+    private ArrayList<Point> losers;
+    private boolean drawPicture = false;
+    private int moveDelay = 7;
+    private int moveCounter = moveDelay;
+    private ArrayList<Point> bombs;
+
     public SnakeEnvironment() {
     }
 
     @Override
-    public void initializeEnvironment() {  
-        this.tofu = new Grid();
-        this.tofu.setColor(Color.RED);
-        this.tofu.setColumns(40);
-        this.tofu.setRows(20);
-        this.tofu.setCellHeight(20);
-        this.tofu.setCellWidth(20);
-        this.tofu.setPosition(new Point(50,100));
-        
-        
-        this.bored = new LOL();
-        this.bored.getBody().add(new Point(5,5));
-        this.bored.getBody().add(new Point(5,4));
-        this.bored.getBody().add(new Point(5,3));
-        this.bored.getBody().add(new Point(4,3));
+    public void initializeEnvironment() {
+        this.grid = new Grid();
+        this.grid.setColor(Color.RED);
+        this.grid.setColumns(40);
+        this.grid.setRows(20);
+        this.grid.setCellHeight(20);
+        this.grid.setCellWidth(20);
+        this.grid.setPosition(new Point(50, 100));
+
+        this.loser = ResourceTools.loadImageFromResource("resources/HAH.jpeg");
+        this.losers = new ArrayList<Point>();
+        this.losers.add(getRandomGridLocation());
+
+        this.tofu = ResourceTools.loadImageFromResource("resources/2008-06-23-tofu.png");
+        this.tofus = new ArrayList<Point>();
+        for (int i = 0; i < 1; i++) {
+            this.tofus.add(getRandomGridLocation());
+        }
+
+        this.apples = new ArrayList<Point>();
+        for (int i = 0; i < 3; i++) {
+            this.apples.add(getRandomGridLocation());
+        }
+
+        this.bombs = new ArrayList<Point>();
+        for (int i = 0; i < 3; i++) {
+            this.bombs.add(getRandomGridLocation());
+        }
+
+        this.snake = new LOL();
+        restart();
+    }
+
+    private Point getRandomGridLocation() {
+        return new Point((int) (Math.random() * this.grid.getColumns()), (int) (Math.random() * this.grid.getRows()));
     }
 
     @Override
-    public void timerTaskHandler() { 
-        if (bored !=null){
-//            bored.move();
-        }
-        
-        if (bored.getDirection() == Diection.RIGHT) {
-                if (bored.getHead().x >= this.tofu.getColumns()) {
-                    bored.getHead().x = 0 ;
-                }
-            } else if (bored.getDirection() == Direction.LEFT) {
-                if (bored.getHead().x <=-1) {
-                    bored.getHead().x = this.tofu.getColumns()-1;
-                }
-            } else if (bored.getDirection() == Direction.UP) {
-                if (bored.getHead().y <= -1) {
-                    bored.getHead().y = this.tofu.getRows()-1;
-                }
-            } else if (bored.getDirection() == Direction.DOWN) {
-                if (bored.getHead().y >= this.tofu.getRows()) {
-                    bored.getHead().y = 0;
+    public void timerTaskHandler() {
+        if (this.gameState == GameState.RUNNING) {
+            if (snake != null) {
+                if (moveCounter <= 0) {
+                    snake.move();
+                    moveCounter = moveDelay;
+                    checkBoredIntersection();
+
+                    if (snake.selfHitTest()) {
+                        this.gameState = GameState.ENDED;
+                    }
+                } else {
+                    moveCounter--;
                 }
             }
-        
+        }
+
+        if (snake.getDirection() == Direction.RIGHT) {
+            if (snake.getHead().x >= this.grid.getColumns()) {
+                snake.getHead().x = 0;
+            }
+        } else if (snake.getDirection() == Direction.LEFT) {
+            if (snake.getHead().x <= -1) {
+                snake.getHead().x = this.grid.getColumns() - 1;
+            }
+        } else if (snake.getDirection() == Direction.UP) {
+            if (snake.getHead().y <= -1) {
+                snake.getHead().y = this.grid.getRows() - 1;
+            }
+        } else if (snake.getDirection() == Direction.DOWN) {
+            if (snake.getHead().y >= this.grid.getRows()) {
+                snake.getHead().y = 0;
+            }
+        }
     }
 
     @Override
     public void keyPressedHandler(KeyEvent e) {
-    if(e.getKeyCode() == KeyEvent.VK_SPACE){
-        this.score += 500000000;
-        }else if (e.getKeyCode() ==KeyEvent.VK_G){
-            bored.kimchi();
-        }else if (e.getKeyCode() ==KeyEvent.VK_UP){
-            bored.setDirection(Direction.UP);
-        }else if (e.getKeyCode() ==KeyEvent.VK_DOWN){
-            bored.setDirection(Direction.DOWN);
-        }else if (e.getKeyCode() ==KeyEvent.VK_LEFT){
-            bored.setDirection(Direction.LEFT);
-        }else if (e.getKeyCode() ==KeyEvent.VK_RIGHT){
-            bored.setDirection(Direction.RIGHT);
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            System.out.println("Game State " + gameState.toString());
+
+            if (gameState == GameState.RUNNING) {
+                gameState = GameState.PAUSED;
+            } else if (gameState == GameState.PAUSED) {
+                gameState = GameState.RUNNING;
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_UP) {
+            if (snake.getDirection() != Direction.DOWN) {
+                snake.setDirection(Direction.UP);
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            if (snake.getDirection() != Direction.UP) {
+                snake.setDirection(Direction.DOWN);
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            if (snake.getDirection() != Direction.RIGHT) {
+                snake.setDirection(Direction.LEFT);
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            if (snake.getDirection() != Direction.LEFT) {
+                snake.setDirection(Direction.RIGHT);
+            }
+        } else if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
+            gameState = GameState.ENDED;
+        } else if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            restart();
         }
+//        if ((e.getKeyCode()== KeyEvent.VK_LEFT)&&(e.getKeyCode()==KeyEvent.VK_RIGHT)) {
+//            
+//        }
+
     }
 
     @Override
@@ -97,21 +162,167 @@ class SnakeEnvironment extends Environment{
 
     @Override
     public void paintEnvironment(Graphics graphics) {
-   if (this.tofu != null){
-       this.tofu.paintComponent(graphics);
-       
-       Point cellLocation;
-       graphics.setColor(Color.pink);
-       if(bored!= null){
-           for (int i = 0; i < bored.getBody().size(); i++) {
-               cellLocation = tofu.getCellPosition(bored.getBody().get(i));
-               graphics.fillOval(cellLocation.x, cellLocation.y, tofu.getCellWidth(), tofu.getCellHeight());
-               
-           }
-       }
+        if (this.grid != null) {
+//            this.grid.paintComponent(graphics);
+
+            for (int i = 0; i < this.apples.size(); i++) {
+                this.apples.get(i);
+                GraphicsPalette.drawApple(graphics, this.grid.getCellPosition(this.apples.get(i)), this.grid.getCellSize());
+            }
+            for (int i = 0; i < this.bombs.size(); i++) {
+                this.bombs.get(i);
+                GraphicsPalette.drawBomb(graphics, this.grid.getCellPosition(this.bombs.get(i)), this.grid.getCellSize(), Color.BLACK);
+            }
+
+            Point cellLocation;
+            if (snake != null) {
+                graphics.setColor(snake.getColor());
+                
+                for (int i = 0; i < snake.getBody().size(); i++) {
+                    cellLocation = grid.getCellPosition(snake.getBody().get(i));
+                    if (i == 0) {
+                        snake.drawHead(graphics, cellLocation, this.grid.getCellSize());
+                    } else {
+                        graphics.fillOval(cellLocation.x, cellLocation.y, grid.getCellWidth(), grid.getCellHeight());
+                    }
+                }
+            }
+
+            if (tofus != null) {
+                for (int i = 0; i < tofus.size(); i++) {
+                    cellLocation = grid.getCellPosition(tofus.get(i));
+                    graphics.drawImage(tofu, cellLocation.x, cellLocation.y, this.grid.getCellHeight(), this.grid.getCellWidth(), this);
+//                    graphics.fillOval( grid.getCellWidth(), grid.getCellHeight());
+                }
+            }
+
+            graphics.setFont(new Font("Calibri", Font.BOLD, 100));
+            graphics.drawString("score = " + this.getScore(), 100, 100);
+            if (drawPicture) {
+                graphics.drawImage(loser, 0, 0, this);
+            }
+            if (gameState == GameState.ENDED) {
+                this.drawPicture = true;
+                graphics.setColor(Color.red);
+                graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+                graphics.drawString("GAME OVER!!!", 50, 450);
+                graphics.drawString("score = " + this.getScore(), 100, 100);
+            }
+
+            if (this.score == 0) {
+                graphics.setColor(Color.red);
+                graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+                graphics.drawString("Level 1", 600, 100);
+            }
+            if (this.score == 50) {
+                graphics.setColor(Color.red);
+                graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+                graphics.drawString("Level 2", 600, 100);
+            }
+            if (this.score == 100) {
+                graphics.setColor(Color.red);
+                graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+                graphics.drawString("Level 3", 600, 100);
+            }
+            if (this.score == 150) {
+                graphics.setColor(Color.red);
+                graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+                graphics.drawString("Level 4", 600, 100);
+            }
+            if (this.score == 200) {
+                graphics.setColor(Color.red);
+                graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+                graphics.drawString("Level 5", 600, 100);
+            }
+            if (this.score == 300) {
+                graphics.setColor(Color.red);
+                graphics.setFont(new Font("Calibri", Font.ITALIC, 100));
+                graphics.drawString("Level infinate", 100, 600);
+            }
+        }
     }
-  
-   graphics.setFont(new Font("Times New Roman", Font.BOLD,60));
-   graphics.drawString("Score: "+ this.score,50,50);
-    }   
+
+    private void restart() {
+        //set the score to 0
+        score = 0;
+
+        //move the snake back
+        snake.getBody().clear();
+        snake.getBody().add(new Point(5, 8));
+        snake.getBody().add(new Point(5, 7));
+        snake.getBody().add(new Point(5, 6));
+        snake.getBody().add(new Point(5, 5));
+        snake.getBody().add(new Point(5, 4));
+
+        //reset the speed
+        this.moveDelay = 8;
+        //reset the gamestate
+        gameState = GameState.PAUSED;
+        this.drawPicture = false;
+
+    }
+
+    private void checkBoredIntersection() {
+//if the snake location is the same as an apple location
+        //ithen grow the smake and remove the apple
+        //later,move apple and make a sound and incresase the score
+        for (int i = 0; i < this.apples.size(); i++) {
+            if (snake.getHead().equals(this.apples.get(i))) {
+//                System.out.println("Apple chomp!!!!!");
+                this.setScore(this.getScore() + 10);
+                this.apples.get(i).setLocation(getRandomGridLocation());
+                //grow the snake
+                this.snake.grow(1);
+            }
+        }
+        for (int i = 0; i < this.bombs.size(); i++) {
+            if (snake.getHead().equals(this.bombs.get(i))) {
+                System.out.println("GameOver");
+                gameState = GameState.ENDED;
+                break;
+            }
+        }
+        for (int i = 0; i < this.tofus.size(); i++) {
+            if (snake.getHead().equals(this.tofus.get(i))) {
+                this.setScore(this.getScore() + 10);
+                this.snake.grow(1);
+                this.tofus.get(i).setLocation(getRandomGridLocation());
+                this.moveDelay++;
+                break;
+            }
+        }
+    }
+
+    /**
+     * @return the score
+     */
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * @param newScore the score to set
+     */
+    public void setScore(int newScore) {
+
+        if ((this.score < 50) && (newScore >= 50)) {
+            this.moveDelay--;
+            this.moveDelay--;
+        } else if ((this.score < 100) && (newScore >= 100)) {
+            this.moveDelay--;
+        } else if ((this.score < 150) && (newScore >= 150)) {
+            this.moveDelay--;
+            this.moveDelay--;
+        } else if ((this.score < 200) && (newScore >= 200)) {
+            this.moveDelay--;
+            this.moveDelay--;
+        } else if ((this.score < 300) && (newScore >= 300)) {
+            this.moveDelay--;
+            this.moveDelay--;
+            this.moveDelay--;
+            this.moveDelay--;
+            this.moveDelay--;
+        }
+        this.score = newScore;
+    }
 }
